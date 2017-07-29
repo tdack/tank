ec11a4a9e3f0c169b6ad24cec3587f506bdbe3bf
Troy Dack <troy@dack.com.au>
2017-07-29 14:32:43 +1000

    Testing sending distance only

--- index.js
+++ index.js
@@ -5,15 +5,6 @@ const port = 3456
 
 var sonarPin1 = new Sonar(1)
 
-app.get('/distance', (req, res) => {
-	console.log(req.url);
-	sonarPin1.read( (duration)=> {
-		var distance = 343.0 * duration / 1000000 * 0.5
-		res.send('' + distance)
-		})
-})
-
-
 app.listen(port, (err) => {
 	if (err) {
 		return console.log('Error:', err)
