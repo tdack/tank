97dde2477bd79f270f0abbede6597bdceac71645
Troy Dack <troy@dack.com.au>
2017-07-29 14:40:20 +1000

    Add /distance route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var distance = require('./routes/distance');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/distance', distance);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/distance.js
+++ routes/distance.js
@@ -9,7 +9,6 @@ router.get('/', function(req, res, next) {
     sonarPin1.read( (duration)=> {
         var dist = 343.0 * duration / 1000000 * 0.5
         var vol = (2.2 - dist) * (1.65 * 1.65) * Math.PI * 1000
-	console.log(vol)
         res.send({
             title: 'Distance',
             distance: dist,
