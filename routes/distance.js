b60b55d1c611c3d24713be133462a1f9a84c274d
Troy Dack <troy@dack.com.au>
2017-07-29 14:47:35 +1000

    Used fixed decimals in calculations

--- routes/distance.js
+++ routes/distance.js
@@ -7,12 +7,11 @@ var sonarPin1 = new Sonar(1)
 /* GET distance  */
 router.get('/', function(req, res, next) {
     sonarPin1.read( (duration)=> {
-        var dist = 343.0 * duration / 1000000 * 0.5
-        var vol = (2.2 - dist) * (1.65 * 1.65) * Math.PI * 1000
+        var dist = (343.0 * duration / 1000000 * 0.5).toFixed(2)
+        var vol = ((2.2 - dist) * (1.65 * 1.65) * Math.PI * 1000).toFixed(3)
         res.send({
-            title: 'Distance',
-            distance: dist.toFixed(2),
-            volume: vol.toFixed(3)
+            distance: dist,
+            volume: vol
 	})
     });
 });
